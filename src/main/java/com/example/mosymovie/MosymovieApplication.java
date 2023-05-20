13d78b6bf7157ede6f4104c4e95b15c8d148bf6a
hanni01 <he3080@gmail.com>
2023-05-20 21:33:23 +0900

    feat: sign up with email function

---
+++ src/main/java/com/example/mosymovie/MosymovieApplication.java
@@ -0,0 +1,13 @@
+package com.example.mosymovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosymovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosymovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mosymovie/MosymovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mosymovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosymovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
